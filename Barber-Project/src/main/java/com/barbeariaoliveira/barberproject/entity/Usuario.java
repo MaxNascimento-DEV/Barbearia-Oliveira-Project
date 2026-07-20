3a0e64523e7a8a4dd5444f7a2db3ced799373b77
MaxNascimento-DEV <maxwellcadete9@gmail.com>
2026-07-20 10:39:15 -0300

    Atualização

--- Barber-Project/src/main/java/com/barbeariaoliveira/barberproject/entity/Usuario.java
+++ Barber-Project/src/main/java/com/barbeariaoliveira/barberproject/entity/Usuario.java
@@ -1,5 +1,35 @@
 package com.barbeariaoliveira.barberproject.entity;
 
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDateTime;
+
+@Table(name = "usuarios")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+
 public class Usuario {
-    
+
+    @id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column
+    private String nome;
+
+    @Column
+    private String telefone;
+
+    @Column
+    private String email;
+
+    @Column
+    private Boolean ativo;
+
+    @Column
+    private LocalDateTime criadoEm;
+
 }
