248bf3f0f06d412ea9c8d75dfa44cbdc02610c9f
MaxNascimento-DEV <maxwellcadete9@gmail.com>
2026-07-31 10:54:49 -0300

    feat: criar estrutura inicial do AgendamentoService

--- Barber-Project/src/main/java/com/barbeariaoliveira/barberproject/service/AgendamentoService.java
+++ Barber-Project/src/main/java/com/barbeariaoliveira/barberproject/service/AgendamentoService.java
@@ -1,4 +1,23 @@
 package com.barbeariaoliveira.barberproject.service;
 
+import com.barbeariaoliveira.barberproject.repository.AgendamentoRepository;
+import com.barbeariaoliveira.barberproject.repository.BarbeiroRepository;
+import com.barbeariaoliveira.barberproject.repository.ServicosRepository;
+import com.barbeariaoliveira.barberproject.repository.UsuarioRepository;
+
 public class AgendamentoService {
+    private final AgendamentoRepository agendamentoRepository;
+    private final UsuarioRepository usuarioRepository;
+    private final BarbeiroRepository barbeiroRepository;
+    private final ServicosRepository servicosRepository;
+
+    public AgendamentoService(AgendamentoRepository agendamentoRepository, UsuarioRepository usuarioRepository, BarbeiroRepository barbeiroRepository, ServicosRepository servicosRepository){
+        this.agendamentoRepository = agendamentoRepository;
+        this.usuarioRepository = usuarioRepository;
+        this.barbeiroRepository = barbeiroRepository;
+        this.servicosRepository = servicosRepository;
+    }
+
+
 }
+
